70bf025903a902e11ff3aafdabab6e3e91ffeee7
Dararith-ux <rith066463799@gmail.com>
2025-06-06 14:16:51 +0700

    new commit

--- src/js/api.js
+++ src/js/api.js
@@ -1,25 +1,34 @@
-"use strict"
+"use strict";
 const api_key = "c57ca76ab731221b15c53979859d9125";
 const url = `https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&language=en-US&page=1`;
+const imgsrc = "https://image.tmdb.org/t/p/w300";
+const cardcontainer = document.getElementById("postercontainer");
 
 fetch(url)
-  .then(res => res.json())
-  .then(data => {
-    const posterDiv = document.getElementById("poster");
+  .then((res) => res.json())
+  .then((data) => {
     const movies = data.results;
-
-    // Clear the container
-    posterDiv.innerHTML = "";
-
-    // Loop through each movie and display the poster
-    movies.forEach(movie => {
-      const img = document.createElement("img");
-      img.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
-      img.alt = movie.title;
-      img.style.width = "200px"; // optional
-      img.style.margin = "10px";
-
-      posterDiv.appendChild(img);
-    });
+    console.log(movies);
+    // Use map and return HTML string, then join it
+    cardcontainer.innerHTML = movies
+      .map((movie) => {
+        return `
+        <div
+  class="flex flex-col w-[300px] h-[500px] bg-gray-900 rounded-lg overflow-hidden shadow-md"
+>
+  <img
+    src="${imgsrc}${movie.poster_path}"
+    alt="${movie.title}"
+    class="w-full h-[400px] object-cover"
+  />
+  <div class="p-2 flex items-center justify-center h-[100px]">
+    <h1 class="text-white text-center text-base font-semibold line-clamp-2">
+      ${movie.title}
+    </h1>
+  </div>
+</div>
+        `;
+      })
+      .join("");
   })
-  .catch(err => console.error(err));
+  .catch((err) => console.error(err));
---
+++ src/js/tempCodeRunnerFile.js
@@ -0,0 +1,1 @@
+const cardcontainer = document.getElementById("postercontainer");
